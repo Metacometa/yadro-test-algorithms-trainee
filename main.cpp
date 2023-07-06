6b98c4673117e403a79a350096be0393247b2a4d
Kirill <kirillstoker@gmail.com>
2023-07-06 20:03:05 +0300

    Change main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 ﻿#include <iostream>
 #include "ViterbiDecoding.h"
+#include "ConvolutionalCoding.h"
 
 int main()
 {
-    ViterbiDecoding vd({1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1});
+    ConvolutionalCoding cc;
+    std::string encoded = cc.coding("11");
+    ConvolutionalCoding::printEncoded(encoded);
 
-    ViterbiDecoding vd1({ 1, 1, 1, 0,  1, 1, 1, 1, 0, 1, 1, 0,  0,1, 0,0, 0,1, 0,1, 1,1 });
 
-    ViterbiDecoding vd2({ 1,1, 1,0, 1,1, 1,1, 0,1, 1,0 ,0,1 ,0,0, 0,1, 0,1, 1,1, });
+
+    ViterbiDecoding vd;
+    std::string vd_path = vd.decoding("11-01-01-11");
+    ViterbiDecoding::printPath(vd_path);
+
+
+
+    /*
+    std::vector<bool> test_bool_1{ 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1 };
+
+    vd_path = vd.decoding(test_bool_1);
+    ViterbiDecoding::printPath(vd_path);
+
+    std::string test_string_1 = "11-01-10-01-00-01-01-11";
+
+    vd_path = vd.decoding(test_string_1);
+    ViterbiDecoding::printPath(vd_path);
+    */
+
+    /*
+    std::string test_string_2 = "11-00-00-11-10";
+    vd_path = vd.decoding(test_string_2);
+    ViterbiDecoding::printPath(vd_path);
+
+    std::string test_string_3 = "11-10-11-11-01-10-01-00-01-01-11";
+    vd_path = vd.decoding(test_string_3);
+    ViterbiDecoding::printPath(vd_path);
+    */
 
 }
 
